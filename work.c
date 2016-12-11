b6c4d177815d7e9cf3fbf19bcecdfcea74950ae6
Dmytro Starosud <d.starosud@gmail.com>
2016-12-11 12:11:07 +0200

    socketpair

---
+++ work.c
@@ -0,0 +1,21 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <stdbool.h>
+
+#define R0(X) do { if ((X) !=  0) { perror(#X); exit(errno); } } while (0)
+#define R1(X) do { if ((X) == -1) { perror(#X); exit(errno); } } while (0)
+
+int main()
+{
+    int ss[2];
+    R0(socketpair(AF_LOCAL, SOCK_STREAM, 0, ss));
+    pid_t child_pid;
+    R1(child_pid = fork());
+    R0(close(!child_pid));
+    while (true);
+}
